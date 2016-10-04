1d214504578008af110e10f40659b097d8c7ae52
Wybe <wybe@ruurdwestra.nl>
2016-10-04 14:24:16 +0200

    + Robots can now be selected. + Right click will send move commands for yellow robots.

--- src/rqt_world_view/world_view.py
+++ src/rqt_world_view/world_view.py
@@ -2,19 +2,22 @@ import os
 import rospy
 import rospkg
 import actionlib
+import roslib
+roslib.load_manifest("roboteam_msgs")
 
 import math
 
 from qt_gui.plugin import Plugin
-from python_qt_binding import loadUi, QtCore, QtGui
+from python_qt_binding import loadUi, QtCore, QtGui, QtWidgets
 from python_qt_binding.QtCore import pyqtSignal
-from python_qt_binding.QtWidgets import QWidget, QLabel, QGraphicsScene, QGraphicsItemGroup, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsTextItem
+from python_qt_binding.QtWidgets import QWidget, QLabel, QGraphicsScene, QGraphicsItemGroup, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsTextItem, QGraphicsRectItem
 
 from roboteam_msgs.msg import World as WorldMessage
 from roboteam_msgs.msg import GeometryData as GeometryMessage
 from roboteam_msgs.msg import SteeringAction, SteeringGoal
 
 from field_graphics_view import FieldGraphicsView
+from field_graphics_scene import FieldGraphicsScene
 
 
 BOT_DIAMETER = 180 # Diameter of the bots in mm.
@@ -24,11 +27,21 @@ class WorldViewPlugin(Plugin):
 
     # Qt signal for when the world state changes.
     worldstate_signal = pyqtSignal(WorldMessage)
+    # Qt signal for when the graphics calibration changes.
+    geometry_signal = pyqtSignal(GeometryMessage)
 
     # Graphic representations of the blue and yellow robots.
     # QGraphicsItemGroup.
     robots_blue = {}
     robots_yellow = {}
+
+    # References to the selection circles.
+    robots_blue_selectors = {}
+    robots_yellow_selectors = {}
+
+    # List of selected robot id's.
+    robots_blue_selected = []
+    robots_yellow_selected = []
 
     ball = QGraphicsEllipseItem(0, 0, 50, 50)
 
@@ -36,6 +49,8 @@ class WorldViewPlugin(Plugin):
     # Field size in mm.
     field_width = 9000
     field_height = 6000
+
+    field_background = QGraphicsRectItem(-field_width/2, -field_height/2, field_width, field_height)
 
 
     def __init__(self, context):
@@ -77,7 +92,6 @@ class WorldViewPlugin(Plugin):
         # Add widget to the user interface
         context.add_widget(self.widget)
 
-
         # Subscribe to the world state.
         self.worldstate_sub = rospy.Subscriber("world_state", WorldMessage, self.worldstate_callback)
 
@@ -87,32 +101,50 @@ class WorldViewPlugin(Plugin):
         # Create the steering action client.
         self.client = actionlib.SimpleActionClient("steering", SteeringAction)
 
-        # Connect the signal sent by the worldstate callback to the message slot.
-        self.worldstate_signal.connect(self.worldstate_slot)
-
-
-
         # ---- Field view initialization ----
 
-        self.scene = QGraphicsScene();
+        self.scene = FieldGraphicsScene();
         self.fieldview = FieldGraphicsView()
         self.fieldview.setScene(self.scene)
 
         # Add to the main window.
         self.widget.layout().addWidget(self.fieldview)
 
-        self.reset_view()
+        #self.fieldview.setDragMode(FieldGraphicsView.RubberBandDrag)
+
+        self.scene.clear()
+
+        # Add the field to the scene.
+        self.scene.addItem(self.field_background)
+        self.field_background.setBrush(QtGui.QBrush(QtGui.QColor(0, 200, 50)))
 
         # Add the ball to the scene.
         self.scene.addItem(self.ball)
-
         # Ball = orange.
         self.ball.setBrush(QtGui.QBrush(QtGui.QColor(255, 100, 0)))
+
+        # Scale the scene so that it fits into the view area.
+        self.fieldview.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
         # ---- /Field view initialization ----
 
         self.font = QtGui.QFont()
         self.font.setPixelSize(BOT_DIAMETER*0.8)
+
+        # ---- Signal connections ----
+
+        # Connect the signal sent by the worldstate callback to the message slot.
+        self.worldstate_signal.connect(self.worldstate_slot)
+        # Connect the Geometry callback to the Geometry slot.
+        self.geometry_signal.connect(self.geometry_slot)
+
+        # Connect the scene's selectionChanged signal.
+        self.scene.selectionChanged.connect(self.selection_changed_slot)
+
+        # Connect the scenes right click signal.
+        self.scene.right_click_signal.connect(self.view_right_clicked_slot)
+
+        # ---- /Signal connections ----
 
 
     def shutdown_plugin(self):
@@ -135,15 +167,6 @@ class WorldViewPlugin(Plugin):
         # This will enable a setting button (gear icon) in each dock widget title bar
         # Usually used to open a modal configuration dialog
 
-    def reset_view(self):
-        self.scene.clear()
-
-        # Draw the background.
-        self.scene.addRect(-self.field_width/2, -self.field_height/2, self.field_width, self.field_height, pen=QtGui.QPen(), brush=QtGui.QBrush(QtCore.Qt.green))
-
-        # Scale the scene so that it fits into the view area.
-        self.fieldview.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
-
 
     # Is called when a worldstate message is received.
     def worldstate_callback(self, message):
@@ -159,21 +182,23 @@ class WorldViewPlugin(Plugin):
         # Process the blue bots.
         for bot in message.robots_blue:
             if not bot.id in self.robots_blue:
-                self.robots_blue[bot.id] = self.create_new_robot(bot.id, QtGui.QColor(0, 100, 255))
+                (self.robots_blue[bot.id], self.robots_blue_selectors[bot.id]) = \
+                        self.create_new_robot(bot.id, QtGui.QColor(0, 100, 255))
                 self.scene.addItem(self.robots_blue[bot.id])
 
             screen_bot = self.robots_blue[bot.id]
-            screen_bot.setPos(bot.pos.x, bot.pos.y)
+            screen_bot.setPos(bot.pos.x, -bot.pos.y)
             screen_bot.setRotation(-math.degrees(bot.w))
 
         # Draw the yellow bots.
         for bot in message.robots_yellow:
             if not bot.id in self.robots_yellow:
-                self.robots_yellow[bot.id] = self.create_new_robot(bot.id, QtGui.QColor(255, 255, 0))
+                (self.robots_yellow[bot.id], self.robots_yellow_selectors[bot.id]) = \
+                        self.create_new_robot(bot.id, QtGui.QColor(255, 255, 0))
                 self.scene.addItem(self.robots_yellow[bot.id])
 
             screen_bot = self.robots_yellow[bot.id]
-            screen_bot.setPos(bot.pos.x, bot.pos.y)
+            screen_bot.setPos(bot.pos.x, -bot.pos.y)
             screen_bot.setRotation(-math.degrees(bot.w))
 
         # Scale the scene so that it fits into the view area.
@@ -182,21 +207,73 @@ class WorldViewPlugin(Plugin):
 
 
     def geometry_callback(self, message):
+        # Send signal to qt thread.
+        self.geometry_signal.emit(message)
+
+
+    def geometry_slot(self, message):
         self.field_width = message.field.field_width
         self.field_length = message.field.field_length
 
-        #TODO: Resize the field rectangle.
+        # Resize the field background.
+        #self.field_background.setRect(-self.field_width/2, -self.field_length/2, self.field_width, self.field_length)
 
         rospy.loginfo("Field width: %i", self.field_width)
         rospy.loginfo("Field length: %i", self.field_length)
 
 
+    # Slot for the scenes selectionChanged signal.
+    def selection_changed_slot(self):
+        # Clear the selection lists.
+        del self.robots_blue_selected[:]
+        del self.robots_yellow_selected[:]
+
+        for bot_id, selector in self.robots_blue_selectors.iteritems():
+            if selector.isSelected():
+                selector.setVisible(True)
+                self.robots_blue_selected.append(bot_id)
+            else:
+                selector.setVisible(False)
+
+        for bot_id, selector in self.robots_yellow_selectors.iteritems():
+            if selector.isSelected():
+                selector.setVisible(True)
+                self.robots_yellow_selected.append(bot_id)
+            else:
+                selector.setVisible(False)
+
+
+    # Called when the view is right clicked.
+    def view_right_clicked_slot(self, event):
+        #TODO: Only send actions when there is a server connected.
+
+        for bot_id in self.robots_yellow_selected:
+            goal_pos = event.scenePos()
+
+            goal = SteeringGoal()
+            goal.robot_id = bot_id
+
+            # The message is in meters, on screen it is in mm.
+            # So divide by 1000.
+            goal.x = goal_pos.x()/1000.0
+            goal.y = -goal_pos.y()/1000.0
+
+            self.client.send_goal(goal)
+            self.client.wait_for_result(rospy.Duration.from_sec(1.0))
+
+
+
     # Creates a QGraphicsItemGroup that represents a robot.
     # Takes an integer as bot id.
     # Takes a QColor to color the bot with.
-    # Returns the QGraphicsItemGroup.
+    # Returns (QGraphicsItemGroup, QGraphicsEllipseItem)
+    # Returns the QGraphicsItemGroup representing the bot
+    # plus the QGraphicsEllipse used for indicating the bot is selected.
     def create_new_robot(self, bot_id, color):
         bot = QGraphicsItemGroup()
+
+        # Make the bot selectable.
+        bot.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
 
         ellipse = QGraphicsEllipseItem(-BOT_DIAMETER/2, -BOT_DIAMETER/2, BOT_DIAMETER, BOT_DIAMETER)
         ellipse.setBrush(QtGui.QBrush(color))
@@ -213,7 +290,18 @@ class WorldViewPlugin(Plugin):
         id_text.setPos(-BOT_DIAMETER/3,-BOT_DIAMETER/2)
         bot.addToGroup(id_text)
 
-        return bot
+        selection_pen = QtGui.QPen(QtGui.QColor(0, 255, 255))
+        selection_pen.setWidth(10)
+
+        # Pixels for the selection circle to be bigger than the bot.
+        offset = BOT_DIAMETER * 0.3
+
+        selector_ellipse = QGraphicsEllipseItem(-((BOT_DIAMETER/2)+offset/2), -((BOT_DIAMETER/2)+offset/2), BOT_DIAMETER + offset, BOT_DIAMETER + offset)
+        selector_ellipse.setPen(selection_pen)
+        selector_ellipse.setVisible(False)
+        bot.addToGroup(selector_ellipse)
+
+        return (bot, selector_ellipse)
 
         #id_text = self.scene.addText(str(bot.id), self.font)
         #id_text.setPos(bot.pos.x - BOT_DIAMETER*0.2, -(bot.pos.y - BOT_DIAMETER*0.5))
