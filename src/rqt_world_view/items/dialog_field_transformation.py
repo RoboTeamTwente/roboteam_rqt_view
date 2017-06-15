d1e0ec411bcec3293e269fad766ced46605de769
Wybe <wybe@ruurdwestra.nl>
2017-06-15 11:32:26 +0200

    + Transform config dialog now loads values on opening. + Added reset button to transform dialog.

--- src/rqt_world_view/items/dialog_field_transformation.py
+++ src/rqt_world_view/items/dialog_field_transformation.py
@@ -55,7 +55,50 @@ class DialogFieldTransformation(QtWidgets.QDialog):
         self.layout().addWidget(self.apply_button, 4, 2, 1, 2)
         self.apply_button.clicked.connect(self.apply_config)
 
+        self.reset_button = QtWidgets.QPushButton("Reset")
+        self.layout().addWidget(self.reset_button, 4, 0, 1, 2)
+        self.reset_button.clicked.connect(self.reset_config)
+
         # ---- /Wigets ----
+
+    def show(self):
+        super(DialogFieldTransformation, self).show()
+
+        enabled = rospy.get_param(PARAM_GROUP + "enabled", False)
+        if isinstance(enabled, bool):
+            self.enabled_checkbox.setChecked(enabled)
+
+        rotate = rospy.get_param(PARAM_GROUP + "rotate", False)
+        if isinstance(rotate, bool):
+            self.rotate_checkbox.setChecked(rotate)
+
+        top = rospy.get_param(PARAM_GROUP + "offset/top", 0.0)
+        try:
+            top = float(top)
+        except ValueError:
+            top = 0.0
+        self.top_edit.setValue(top)
+
+        bottom = rospy.get_param(PARAM_GROUP + "offset/bottom", 0.0)
+        try:
+            bottom = float(bottom)
+        except ValueError:
+            bottom = 0.0
+        self.bottom_edit.setValue(bottom)
+
+        left = rospy.get_param(PARAM_GROUP + "offset/left", 0.0)
+        try:
+            left = float(left)
+        except ValueError:
+            left = 0.0
+        self.left_edit.setValue(left)
+
+        right = rospy.get_param(PARAM_GROUP + "offset/right", 0.0)
+        try:
+            right = float(right)
+        except ValueError:
+            right = 0.0
+        self.right_edit.setValue(right)
 
     def apply_config(self):
         rospy.set_param(PARAM_GROUP + "enabled", self.enabled_checkbox.isChecked())
@@ -64,3 +107,14 @@ class DialogFieldTransformation(QtWidgets.QDialog):
         rospy.set_param(PARAM_GROUP + "offset/bottom", self.bottom_edit.value())
         rospy.set_param(PARAM_GROUP + "offset/left", self.left_edit.value())
         rospy.set_param(PARAM_GROUP + "offset/right", self.right_edit.value())
+
+    def reset_config(self):
+        self.enabled_checkbox.setChecked(False)
+        self.rotate_checkbox.setChecked(False)
+
+        self.top_edit.setValue(0.0)
+        self.bottom_edit.setValue(0.0)
+        self.left_edit.setValue(0.0)
+        self.right_edit.setValue(0.0)
+
+        self.apply_config()
