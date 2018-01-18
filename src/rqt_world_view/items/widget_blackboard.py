9ffce04b19c8a0c1a73dbcbc99d3fa741188bf61
RoboTeam PC <roboteampc@gmail.com>
2018-01-18 14:28:32 +0100

    Fixed the scrolling issue

--- src/rqt_world_view/items/widget_blackboard.py
+++ src/rqt_world_view/items/widget_blackboard.py
@@ -146,6 +146,19 @@ class WidgetBlackboard(QFrame):
 
         del self.blackboard_items[item_id]
 
+# A QComboBox that cannot be changed by scrolling
+# Normally on mouseover, the user can scroll through
+# the options in the combobox. In rqt this can easily
+# screw up the command when scrolling through the list
+# of commands. That is why we prefer to disable it.
+class NonScrollableQComboBox(QComboBox):
+	def __init__(self, *args, **kwargs):
+		super(NonScrollableQComboBox, self).__init__(*args, **kwargs)
+		self.setFocusPolicy(Qt.StrongFocus)
+
+	def wheelEvent(self, *args, **kwargs):
+		# pass the event to the parent so that the box does not block scrolling alltogether
+		return self.parent().wheelEvent(*args, **kwargs)
 
 
 class BlackboardItem():
@@ -159,12 +172,11 @@ class BlackboardItem():
         self.id = item_id
         self.remove_callback = remove_callback
 
-        self.type_widget = QComboBox()
+        self.type_widget = NonScrollableQComboBox()
         self.type_widget.insertItem(0, "String")
         self.type_widget.insertItem(1, "Double")
         self.type_widget.insertItem(2, "Int")
         self.type_widget.insertItem(3, "Bool")
-        self.type_widget.setFocusPolicy(Qt.ClickFocus)
 
         # ---- Value widget ----
 
